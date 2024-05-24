ad91334dba74c3e1b6b3526ba9833d64f4cafb0a
João Vitor Soares <jvfs12th@gmail.com>
2024-05-24 11:19:35 -0300

    :tada: init: Inicia projeto

---
+++ src/test/java/com/group/vitalmedapi/VitalmedapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.vitalmedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalmedapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4ef561ec6cc07fce2b7cadea458a9d095db277ca
João Vitor Soares <jvfs12th@gmail.com>
2024-05-24 14:43:30 -0300

    :sparkles: feat: Adiciona método de criar Funcionário

--- src/main/java/com/group/vitalmedapi/controllers/FuncionarioController.java
+++ src/main/java/com/group/vitalmedapi/controllers/FuncionarioController.java
@@ -13,6 +13,8 @@ import com.group.vitalmedapi.models.Funcionario;
 import com.group.vitalmedapi.services.FuncionarioService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -32,4 +34,8 @@ public class FuncionarioController {
        return ResponseEntity.status(HttpStatus.OK).body(funcionarioService.findById(id));
     }
     
+    @PostMapping("/add")
+    public ResponseEntity<Funcionario> addFuncionario(@RequestBody Funcionario funcionario) {
+       return ResponseEntity.status(HttpStatus.CREATED).body(funcionarioService.addFuncionario(funcionario));
+    }
 }

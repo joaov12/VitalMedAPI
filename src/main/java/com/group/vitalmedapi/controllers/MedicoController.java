86257af054bae2cf2693ebfead55b15a4efab103
João Vitor Soares <jvfs12th@gmail.com>
2024-05-26 20:45:35 -0300

    :sparkles: feat: Adiciona método de criar Medicos

--- src/main/java/com/group/vitalmedapi/controllers/MedicoController.java
+++ src/main/java/com/group/vitalmedapi/controllers/MedicoController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,11 @@ public class MedicoController {
     public ResponseEntity<Optional<Medico>> getMedicoById(@PathVariable("id") Long id) {
        return ResponseEntity.status(HttpStatus.OK).body(medicoService.findById(id));
     }
+
+    @PostMapping("/add")
+    public ResponseEntity<Medico> addMedico(@RequestBody Medico medico) {
+       return ResponseEntity.status(HttpStatus.CREATED).body(medicoService.addMedico(medico));
+    }
+
     
 }

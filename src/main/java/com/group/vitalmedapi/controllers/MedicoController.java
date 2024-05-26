a177e6fb55c098a00c85dbc23e102105e8c0ff49
João Vitor Soares <jvfs12th@gmail.com>
2024-05-26 20:47:41 -0300

    :sparkles: feat: Adiciona método de editar Medicos

--- src/main/java/com/group/vitalmedapi/controllers/MedicoController.java
+++ src/main/java/com/group/vitalmedapi/controllers/MedicoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,11 @@ public class MedicoController {
     @PostMapping("/add")
     public ResponseEntity<Medico> addMedico(@RequestBody Medico medico) {
        return ResponseEntity.status(HttpStatus.CREATED).body(medicoService.addMedico(medico));
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Medico> editMedico(@RequestBody Medico medico) {
+       return ResponseEntity.status(HttpStatus.OK).body(medicoService.updateMedico(medico));
     }
 
     

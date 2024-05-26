f114355ea78f16f43ba5903cff981fc182884554
João Vitor Soares <jvfs12th@gmail.com>
2024-05-26 20:28:11 -0300

    :bug: fix: Corrige erro de importação em Funcionarios

--- src/main/java/com/group/vitalmedapi/models/Funcionario.java
+++ src/main/java/com/group/vitalmedapi/models/Funcionario.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 
 @Entity

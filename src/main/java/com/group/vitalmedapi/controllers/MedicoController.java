be570039c91df04b278763a00fc4b06b75a8e915
João Vitor Soares <jvfs12th@gmail.com>
2024-05-26 20:49:21 -0300

    :sparkles: feat: Adiciona método de deletar Medicos

--- src/main/java/com/group/vitalmedapi/controllers/MedicoController.java
+++ src/main/java/com/group/vitalmedapi/controllers/MedicoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,10 @@ public class MedicoController {
        return ResponseEntity.status(HttpStatus.OK).body(medicoService.updateMedico(medico));
     }
 
-    
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteMedico(@PathVariable("id") Long id) {
+       medicoService.deleteMedico(id); 
+       return ResponseEntity.status(HttpStatus.OK).body("Medico deletado com sucesso");
+    }
+
 }

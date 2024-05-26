1b625a897cf695dc0f8c42618f4d42fcd0effe12
João Vitor Soares <jvfs12th@gmail.com>
2024-05-26 20:25:55 -0300

    :construction_worker: build: Cria modelo Medico

---
+++ src/main/java/com/group/vitalmedapi/models/Medico.java
@@ -0,0 +1,37 @@
+package com.group.vitalmedapi.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "medicos")
+public class Medico extends Funcionario {
+    
+    @Column(nullable = false, updatable = false)
+    private String crm;
+
+
+    public Medico(){}
+    public Medico(String nome, Date dataNascimento, Date dataContratacao, double salario, String telefone,
+            String endereco, String crm) {
+        super(nome, dataNascimento, dataContratacao, salario, telefone, endereco);
+        this.crm = crm;
+    }
+
+    public Medico(String crm) {
+        this.crm = crm;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    
+}

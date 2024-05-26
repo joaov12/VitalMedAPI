880c791043fb8354ee61a10dbf614cf19eaab428
João Vitor Soares <jvfs12th@gmail.com>
2024-05-26 20:40:00 -0300

    :sparkles: feat: Adiciona método de buscar todos os médicos

--- src/main/java/com/group/vitalmedapi/controllers/MedicoController.java
+++ src/main/java/com/group/vitalmedapi/controllers/MedicoController.java
@@ -1,15 +1,27 @@
 package com.group.vitalmedapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.group.vitalmedapi.services.FuncionarioService;
+import com.group.vitalmedapi.models.Medico;
+import com.group.vitalmedapi.services.MedicoService;
 
 @RestController
 @RequestMapping("/funcionarios")
 public class MedicoController {
     @Autowired
-    FuncionarioService funcionarioService;
+    MedicoService medicoService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Medico>> getAllMedicos() {
+        return ResponseEntity.status(HttpStatus.OK).body(medicoService.findAll());
+    }
+
     
 }

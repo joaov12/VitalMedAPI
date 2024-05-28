f31150a3afc55b255267a60a1c284feef791959b
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 20:53:21 -0300

    :sparkles: feat: Adiciona método de criar pacientes

--- src/main/java/com/group/vitalmedapi/controllers/PacienteController.java
+++ src/main/java/com/group/vitalmedapi/controllers/PacienteController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,4 +34,14 @@ public class PacienteController {
        return ResponseEntity.status(HttpStatus.OK).body(pacienteService.findById(id));
     }
 
+    @PostMapping("/add")
+    public ResponseEntity<Paciente> addPaciente(@RequestBody Paciente paciente) {
+       return ResponseEntity.status(HttpStatus.CREATED).body(pacienteService.addPaciente(paciente));
+    }
+
+
+
+
+
+    
 }

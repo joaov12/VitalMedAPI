a276f2cc131543d52053f220ded921b10de688c9
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 20:51:36 -0300

    :sparkles: feat: Adiciona método de buscar pacientes por id

--- src/main/java/com/group/vitalmedapi/controllers/PacienteController.java
+++ src/main/java/com/group/vitalmedapi/controllers/PacienteController.java
@@ -1,11 +1,13 @@
 package com.group.vitalmedapi.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,11 @@ public class PacienteController {
     @GetMapping("/all")
     public ResponseEntity<List<Paciente>> getAllPacientes() {
         return ResponseEntity.status(HttpStatus.OK).body(pacienteService.findAll());
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Optional<Paciente>> getPacienteById(@PathVariable("id") Long id) {
+       return ResponseEntity.status(HttpStatus.OK).body(pacienteService.findById(id));
     }
 
 }

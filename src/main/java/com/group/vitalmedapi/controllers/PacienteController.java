0071d0341cfba605f0a830c20ed1d31f8c12688f
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 20:50:27 -0300

    :sparkles: feat: Adiciona método de buscar todos os pacientes

--- src/main/java/com/group/vitalmedapi/controllers/PacienteController.java
+++ src/main/java/com/group/vitalmedapi/controllers/PacienteController.java
@@ -1,9 +1,15 @@
 package com.group.vitalmedapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.group.vitalmedapi.models.Paciente;
 import com.group.vitalmedapi.services.PacienteService;
 
 @RestController
@@ -12,4 +18,11 @@ public class PacienteController {
     
     @Autowired
     PacienteService pacienteService;
+
+    
+    @GetMapping("/all")
+    public ResponseEntity<List<Paciente>> getAllPacientes() {
+        return ResponseEntity.status(HttpStatus.OK).body(pacienteService.findAll());
+    }
+
 }

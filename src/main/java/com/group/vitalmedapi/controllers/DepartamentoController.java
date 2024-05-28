d7d30c145b85ffcee8be5d3c15c0fe518099b512
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 19:44:34 -0300

    :sparkles: feat: Adiciona método de buscar todos os departamentos

--- src/main/java/com/group/vitalmedapi/controllers/DepartamentoController.java
+++ src/main/java/com/group/vitalmedapi/controllers/DepartamentoController.java
@@ -1,18 +1,27 @@
 package com.group.vitalmedapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.group.vitalmedapi.models.Departamento;
 import com.group.vitalmedapi.services.DepartamentoService;
 
 @RestController
 @RequestMapping("departamentos")
 public class DepartamentoController {
-    
+
     @Autowired
     DepartamentoService departamentoService;
 
-    
+    @GetMapping("/all")
+    public ResponseEntity<List<Departamento>> getAllDepartamentos() {
+        return ResponseEntity.status(HttpStatus.OK).body(departamentoService.findAll());
+    }
 
 }

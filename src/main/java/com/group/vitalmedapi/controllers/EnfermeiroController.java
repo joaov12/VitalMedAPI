c55c6059190638cae4f5428c2a3f361cd137a1e7
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 15:39:09 -0300

    :sparkles: feat: Adiciona método de deletar enfermeiros

--- src/main/java/com/group/vitalmedapi/controllers/EnfermeiroController.java
+++ src/main/java/com/group/vitalmedapi/controllers/EnfermeiroController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,4 +44,10 @@ public class EnfermeiroController {
        return ResponseEntity.status(HttpStatus.OK).body(enfermeiroService.updateEnfermeiro(enfermeiro));
     }
 
+        
+   @DeleteMapping("/delete/{id}")
+   public ResponseEntity<?> deleteEnfermeiro(@PathVariable("id") Long id) {
+      enfermeiroService.deleteEnfermeiro(id); 
+      return ResponseEntity.status(HttpStatus.OK).body("Enfermeiro deletado com sucesso"); // Todo: Mudar a mensagem, deixar uma padrão
+   }
 }

d175c38dee6a61e7e7558f061c1fc0b7e3ac67a4
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 19:48:00 -0300

    :sparkles: feat: Adiciona método de criar departamentos

--- src/main/java/com/group/vitalmedapi/controllers/DepartamentoController.java
+++ src/main/java/com/group/vitalmedapi/controllers/DepartamentoController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,10 @@ public class DepartamentoController {
     @GetMapping("/find/{id}")
     public ResponseEntity<Optional<Departamento>> getDepartamentoById(@PathVariable("id") Long id) {
        return ResponseEntity.status(HttpStatus.OK).body(departamentoService.findById(id));
+    }
+    
+    @PostMapping("/add")
+    public ResponseEntity<Departamento> addDepartamento(@RequestBody Departamento departamento) {
+       return ResponseEntity.status(HttpStatus.CREATED).body(departamentoService.addDepartamento(departamento));
     }
 }

d20bcc1f9f5addec4e4819ab7167a9ec9f5f88e7
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 19:49:53 -0300

    :sparkles: feat: Adiciona método de editar departamentos

--- src/main/java/com/group/vitalmedapi/controllers/DepartamentoController.java
+++ src/main/java/com/group/vitalmedapi/controllers/DepartamentoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,4 +38,10 @@ public class DepartamentoController {
     public ResponseEntity<Departamento> addDepartamento(@RequestBody Departamento departamento) {
        return ResponseEntity.status(HttpStatus.CREATED).body(departamentoService.addDepartamento(departamento));
     }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Departamento> editDepartamento(@RequestBody Departamento departamento) {
+       return ResponseEntity.status(HttpStatus.OK).body(departamentoService.updateDepartamento(departamento));
+    }
+
 }

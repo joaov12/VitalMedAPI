3c2a0266aafeeaa68868e7c5e7d94dae69cc6e87
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 20:56:11 -0300

    :sparkles: feat: Adiciona método de deletar pacientes

--- src/main/java/com/group/vitalmedapi/controllers/PacienteController.java
+++ src/main/java/com/group/vitalmedapi/controllers/PacienteController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,8 +46,9 @@ public class PacienteController {
        return ResponseEntity.status(HttpStatus.OK).body(pacienteService.updatePaciente(paciente));
     }
 
-
-
-
-
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deletePaciente(@PathVariable("id") Long id) {
+       pacienteService.deletePaciente(id); 
+       return ResponseEntity.status(HttpStatus.OK).body("Paciente deletado com sucesso");
+    }
 }

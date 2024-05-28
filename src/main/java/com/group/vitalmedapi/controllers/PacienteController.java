c89f0563d07d77ac0a74b7db426d3842902a4e1c
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 20:54:38 -0300

    :sparkles: feat: Adiciona método de editar pacientes

--- src/main/java/com/group/vitalmedapi/controllers/PacienteController.java
+++ src/main/java/com/group/vitalmedapi/controllers/PacienteController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,10 +39,14 @@ public class PacienteController {
     public ResponseEntity<Paciente> addPaciente(@RequestBody Paciente paciente) {
        return ResponseEntity.status(HttpStatus.CREATED).body(pacienteService.addPaciente(paciente));
     }
-
-
-
-
-
     
+    @PutMapping("/edit")
+    public ResponseEntity<Paciente> editPaciente(@RequestBody Paciente paciente) {
+       return ResponseEntity.status(HttpStatus.OK).body(pacienteService.updatePaciente(paciente));
+    }
+
+
+
+
+
 }

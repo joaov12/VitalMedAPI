dc4678ad9877389df8632c89b7ccdf12c22766f9
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 15:32:51 -0300

    :sparkles: feat: Adiciona método de buscar todos os enfermeiros

--- src/main/java/com/group/vitalmedapi/controllers/EnfermeiroController.java
+++ src/main/java/com/group/vitalmedapi/controllers/EnfermeiroController.java
@@ -1,9 +1,15 @@
 package com.group.vitalmedapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.group.vitalmedapi.models.Enfermeiro;
 import com.group.vitalmedapi.services.EnfermeiroService;
 
 @RestController
@@ -11,4 +17,9 @@ import com.group.vitalmedapi.services.EnfermeiroService;
 public class EnfermeiroController {
     @Autowired
     EnfermeiroService enfermeiroService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Enfermeiro>> getAllEnfermeiros() {
+        return ResponseEntity.status(HttpStatus.OK).body(enfermeiroService.findAll());
+    }
 }

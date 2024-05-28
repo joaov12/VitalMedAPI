0fb190b1e8812aa4b148db4cdf869458e56d0977
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 15:36:54 -0300

    :sparkles: feat: Adiciona método de editar enfermeiros

--- src/main/java/com/group/vitalmedapi/controllers/EnfermeiroController.java
+++ src/main/java/com/group/vitalmedapi/controllers/EnfermeiroController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,4 +37,10 @@ public class EnfermeiroController {
     public ResponseEntity<Enfermeiro> addEnfermeiro(@RequestBody Enfermeiro enfermeiro) {
        return ResponseEntity.status(HttpStatus.CREATED).body(enfermeiroService.addEnfermeiro(enfermeiro));
     }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Enfermeiro> editEnfermeiro(@RequestBody Enfermeiro enfermeiro) {
+       return ResponseEntity.status(HttpStatus.OK).body(enfermeiroService.updateEnfermeiro(enfermeiro));
+    }
+
 }

ef5d8f01bcf9e5b8e9453cc4c5827a53cdc9a0e4
João Vitor Soares <jvfs12th@gmail.com>
2024-05-28 15:33:52 -0300

    :sparkles: feat: Adiciona método de buscar enfermeiros por Id

--- src/main/java/com/group/vitalmedapi/controllers/EnfermeiroController.java
+++ src/main/java/com/group/vitalmedapi/controllers/EnfermeiroController.java
@@ -1,11 +1,13 @@
 package com.group.vitalmedapi.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class EnfermeiroController {
     @GetMapping("/all")
     public ResponseEntity<List<Enfermeiro>> getAllEnfermeiros() {
         return ResponseEntity.status(HttpStatus.OK).body(enfermeiroService.findAll());
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Optional<Enfermeiro>> getEnfermeiroById(@PathVariable("id") Long id) {
+       return ResponseEntity.status(HttpStatus.OK).body(enfermeiroService.findById(id));
     }
 }

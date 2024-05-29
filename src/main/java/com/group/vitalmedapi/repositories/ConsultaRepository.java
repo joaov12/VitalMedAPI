fb6c9a8c2b4cc64ee67d976ce1ed695802f47450
João Vitor Soares <jvfs12th@gmail.com>
2024-05-29 18:27:22 -0300

    :wrench: chore: Corrige nome do arquivo de repositório de consultas

--- src/main/java/com/group/vitalmedapi/repositories/ConsultaRepository.java
+++ src/main/java/com/group/vitalmedapi/repositories/ConsultaRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.group.vitalmedapi.models.Consulta;
 
-public interface ConsultasRepository extends JpaRepository<Consulta, Long> {
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
     
 }

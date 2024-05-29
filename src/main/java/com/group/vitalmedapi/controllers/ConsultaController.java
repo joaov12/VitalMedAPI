129891c744f2cca482bf071a18930c7a829b584f
João Vitor Soares <jvfs12th@gmail.com>
2024-05-29 18:34:02 -0300

    :sparkles: feat: Adiciona método de buscar todas consultas

--- src/main/java/com/group/vitalmedapi/controllers/ConsultaController.java
+++ src/main/java/com/group/vitalmedapi/controllers/ConsultaController.java
@@ -1,9 +1,15 @@
 package com.group.vitalmedapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.group.vitalmedapi.models.Consulta;
 import com.group.vitalmedapi.services.ConsultaService;
 
 @RestController
@@ -11,5 +17,10 @@ import com.group.vitalmedapi.services.ConsultaService;
 public class ConsultaController {
     @Autowired
     ConsultaService consultaService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Consulta>> getAllConsultas() {
+        return ResponseEntity.status(HttpStatus.OK).body(consultaService.findAll());
+    }
 
 }

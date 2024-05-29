f272d96f92f3995e4b79170b15dd48a6012a329a
João Vitor Soares <jvfs12th@gmail.com>
2024-05-29 18:38:55 -0300

    :sparkles: feat: Adiciona método de deletar consultas

--- src/main/java/com/group/vitalmedapi/controllers/ConsultaController.java
+++ src/main/java/com/group/vitalmedapi/controllers/ConsultaController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,5 +42,11 @@ public class ConsultaController {
     @PutMapping("/edit")
     public ResponseEntity<Consulta> editConsulta(@RequestBody Consulta consulta) {
        return ResponseEntity.status(HttpStatus.OK).body(consultaService.updateConsulta(consulta));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteConsulta(@PathVariable("id") Long id) {
+       consultaService.deleteConsulta(id); 
+       return ResponseEntity.status(HttpStatus.OK).body("Consulta deletada com sucesso"); 
     }
 }

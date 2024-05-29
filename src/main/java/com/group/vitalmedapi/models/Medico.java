abf9fc5b52436d895fd8025d453f7721c79822f5
João Vitor Soares <jvfs12th@gmail.com>
2024-05-29 18:22:01 -0300

    :recycle: refactor: Altera modelo médico para ter relacionamento com consultas

--- src/main/java/com/group/vitalmedapi/models/Medico.java
+++ src/main/java/com/group/vitalmedapi/models/Medico.java
@@ -1,9 +1,11 @@
 package com.group.vitalmedapi.models;
 
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -12,7 +14,8 @@ public class Medico extends Funcionario {
     
     @Column(nullable = false, updatable = false)
     private String crm;
-
+    @OneToMany(mappedBy = "medico")
+    private List<Consulta> consultas;
 
     public Medico(){}
     public Medico(String nome, Date dataNascimento, Date dataContratacao, double salario, String telefone,
@@ -32,6 +35,11 @@ public class Medico extends Funcionario {
     public void setCrm(String crm) {
         this.crm = crm;
     }
-
+    public List<Consulta> getConsultas() {
+        return consultas;
+    }
+    public void setConsultas(List<Consulta> consultas) {
+        this.consultas = consultas;
+    }
     
 }

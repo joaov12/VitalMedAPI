9cdd6766afad80166976d377bc23e67f79f06277
João Vitor Soares <jvfs12th@gmail.com>
2024-05-29 18:37:39 -0300

    :sparkles: feat: Adiciona método de editar consultas

--- src/main/java/com/group/vitalmedapi/controllers/ConsultaController.java
+++ src/main/java/com/group/vitalmedapi/controllers/ConsultaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +36,10 @@ public class ConsultaController {
     @PostMapping("/add")
     public ResponseEntity<Consulta> addConsulta(@RequestBody Consulta consulta) {
        return ResponseEntity.status(HttpStatus.CREATED).body(consultaService.addConsulta(consulta));
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Consulta> editConsulta(@RequestBody Consulta consulta) {
+       return ResponseEntity.status(HttpStatus.OK).body(consultaService.updateConsulta(consulta));
     }
 }

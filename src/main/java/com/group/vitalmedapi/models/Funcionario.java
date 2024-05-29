38494050596b2b945f2d5539ca612576615b0eae
João Vitor Soares <jvfs12th@gmail.com>
2024-05-29 14:21:49 -0300

    :bug: fix: Corrige bug de chamada recursiva na resposta da requisi~ção

--- src/main/java/com/group/vitalmedapi/models/Funcionario.java
+++ src/main/java/com/group/vitalmedapi/models/Funcionario.java
@@ -2,6 +2,8 @@ package com.group.vitalmedapi.models;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,6 +31,7 @@ public class Funcionario {
     private String Endereco;
     @ManyToOne
     @JoinColumn(name = "departamento_id")
+    @JsonIgnore
     private Departamento departamento;
 
     

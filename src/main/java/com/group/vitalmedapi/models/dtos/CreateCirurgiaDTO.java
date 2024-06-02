fb92e9343de24f50c08f54f10b044b4497dc21d3
João Vitor Soares <jvfs12th@gmail.com>
2024-06-02 19:01:13 -0300

    :construction_worker: build: Cria DTO para agendar cirurgias com Ids

---
+++ src/main/java/com/group/vitalmedapi/models/dtos/CreateCirurgiaDTO.java
@@ -0,0 +1,63 @@
+package com.group.vitalmedapi.models.dtos;
+
+import java.util.Date;
+import java.util.List;
+
+import com.group.vitalmedapi.enums.StatusProcedimentoEnum;
+
+public class CreateCirurgiaDTO {
+    private Long medicoId;
+    private Long pacienteId;
+    private List<Long> enfermeiroIds;
+    private Date dataMarcada;
+    private String motivoDaCirurgia;
+    private StatusProcedimentoEnum statusProcedimento;
+
+    public Long getMedicoId() {
+        return medicoId;
+    }
+
+    public void setMedicoId(Long medicoId) {
+        this.medicoId = medicoId;
+    }
+
+    public Long getPacienteId() {
+        return pacienteId;
+    }
+
+    public void setPacienteId(Long pacienteId) {
+        this.pacienteId = pacienteId;
+    }
+
+    public List<Long> getEnfermeiroIds() {
+        return enfermeiroIds;
+    }
+
+    public void setEnfermeiroIds(List<Long> enfermeiroIds) {
+        this.enfermeiroIds = enfermeiroIds;
+    }
+
+    public Date getDataMarcada() {
+        return dataMarcada;
+    }
+
+    public void setDataMarcada(Date dataMarcada) {
+        this.dataMarcada = dataMarcada;
+    }
+
+    public String getMotivoDaCirurgia() {
+        return motivoDaCirurgia;
+    }
+
+    public void setMotivoDaCirurgia(String motivoDaCirurgia) {
+        this.motivoDaCirurgia = motivoDaCirurgia;
+    }
+
+    public StatusProcedimentoEnum getStatusProcedimento() {
+        return statusProcedimento;
+    }
+
+    public void setStatusProcedimento(StatusProcedimentoEnum statusProcedimento) {
+        this.statusProcedimento = statusProcedimento;
+    }
+}

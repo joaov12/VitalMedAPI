1b325a9f284593baa5dffcaccc1acfd23eaae418
João Vitor Soares <jvfs12th@gmail.com>
2024-06-02 18:51:52 -0300

    :sparkles: feat: Adiciona método de deletar cirurgia

--- src/main/java/com/group/vitalmedapi/controllers/CirurgiaController.java
+++ src/main/java/com/group/vitalmedapi/controllers/CirurgiaController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,10 @@ public class CirurgiaController {
        return ResponseEntity.status(HttpStatus.OK).body(cirurgiaService.updateCirurgia(cirurgia));
     }
 
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteCirurgia(@PathVariable("id") Long id) {
+       cirurgiaService.deleteCirurgia(id); 
+       return ResponseEntity.status(HttpStatus.OK).body("Cirurgia deletada com sucesso"); 
+    }
 
 }

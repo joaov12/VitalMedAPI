095779e95e488ba3306a599bbf1125353abdfe22
João Vitor Soares <jvfs12th@gmail.com>
2024-06-02 18:47:09 -0300

    :sparkles: feat: Adiciona método de buscar todas cirurgias

--- src/main/java/com/group/vitalmedapi/controllers/CirurgiaController.java
+++ src/main/java/com/group/vitalmedapi/controllers/CirurgiaController.java
@@ -1,9 +1,15 @@
 package com.group.vitalmedapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.group.vitalmedapi.models.Cirurgia;
 import com.group.vitalmedapi.services.CirurgiaService;
 
 @RestController
@@ -11,6 +17,11 @@ import com.group.vitalmedapi.services.CirurgiaService;
 public class CirurgiaController {
     
     @Autowired
-    CirurgiaService cirurgiaServiceService;
+    CirurgiaService cirurgiaService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Cirurgia>> getAllCirurgias() {
+        return ResponseEntity.status(HttpStatus.OK).body(cirurgiaService.findAll());
+    }
 
 }

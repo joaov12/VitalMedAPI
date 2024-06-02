cc28f3ff6714b79b7289cbe5ac578486048c9803
João Vitor Soares <jvfs12th@gmail.com>
2024-06-02 18:49:36 -0300

    :sparkles: feat: Adiciona método de criar cirurgia

--- src/main/java/com/group/vitalmedapi/controllers/CirurgiaController.java
+++ src/main/java/com/group/vitalmedapi/controllers/CirurgiaController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,11 @@ public class CirurgiaController {
     @GetMapping("/find/{id}")
     public ResponseEntity<Optional<Cirurgia>> getCirurgiaById(@PathVariable("id") Long id) {
        return ResponseEntity.status(HttpStatus.OK).body(cirurgiaService.findById(id));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Cirurgia> addCirurgia(@RequestBody Cirurgia cirurgia) {
+       return ResponseEntity.status(HttpStatus.CREATED).body(cirurgiaService.addCirurgia(cirurgia));
     }
 
 

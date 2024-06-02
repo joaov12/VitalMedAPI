feb325409a5758acdde4f66f9aad304fb506ba54
João Vitor Soares <jvfs12th@gmail.com>
2024-06-02 18:50:55 -0300

    :sparkles: feat: Adiciona método de editar cirurgia

--- src/main/java/com/group/vitalmedapi/controllers/CirurgiaController.java
+++ src/main/java/com/group/vitalmedapi/controllers/CirurgiaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,11 @@ public class CirurgiaController {
     @PostMapping("/add")
     public ResponseEntity<Cirurgia> addCirurgia(@RequestBody Cirurgia cirurgia) {
        return ResponseEntity.status(HttpStatus.CREATED).body(cirurgiaService.addCirurgia(cirurgia));
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Cirurgia> editCirurgia(@RequestBody Cirurgia cirurgia) {
+       return ResponseEntity.status(HttpStatus.OK).body(cirurgiaService.updateCirurgia(cirurgia));
     }
 
 

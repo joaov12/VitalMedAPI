f102d37780427e755459b21f1684823d4084f30e
João Vitor Soares <jvfs12th@gmail.com>
2024-06-02 18:48:20 -0300

    :sparkles: feat: Adiciona método de buscar cirurgias por Id

--- src/main/java/com/group/vitalmedapi/controllers/CirurgiaController.java
+++ src/main/java/com/group/vitalmedapi/controllers/CirurgiaController.java
@@ -1,11 +1,13 @@
 package com.group.vitalmedapi.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,11 @@ public class CirurgiaController {
     public ResponseEntity<List<Cirurgia>> getAllCirurgias() {
         return ResponseEntity.status(HttpStatus.OK).body(cirurgiaService.findAll());
     }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Optional<Cirurgia>> getCirurgiaById(@PathVariable("id") Long id) {
+       return ResponseEntity.status(HttpStatus.OK).body(cirurgiaService.findById(id));
+    }
+
 
 }

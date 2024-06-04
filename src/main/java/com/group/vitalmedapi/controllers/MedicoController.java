9d19d2243fba947f7050279a4f839d7d4913e5d1
João Vitor Soares <jvfs12th@gmail.com>
2024-06-04 15:00:24 -0300

    :sparkles: feat: Adiciona documentação do swagger em Médicos

--- src/main/java/com/group/vitalmedapi/controllers/MedicoController.java
+++ src/main/java/com/group/vitalmedapi/controllers/MedicoController.java
@@ -18,32 +18,39 @@ import org.springframework.web.bind.annotation.RestController;
 import com.group.vitalmedapi.models.Medico;
 import com.group.vitalmedapi.services.MedicoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
     @Autowired
     MedicoService medicoService;
 
+    @Operation(summary = "Obter todos os Médicos", description = "Retorna uma lista de todos os médicos cadastrados")
     @GetMapping("/all")
     public ResponseEntity<List<Medico>> getAllMedicos() {
         return ResponseEntity.status(HttpStatus.OK).body(medicoService.findAll());
     }
 
+    @Operation(summary = "Obter médico por ID", description = "Retorna um médico com base no ID fornecido")
     @GetMapping("/find/{id}")
     public ResponseEntity<Optional<Medico>> getMedicoById(@PathVariable("id") Long id) {
        return ResponseEntity.status(HttpStatus.OK).body(medicoService.findById(id));
     }
 
+    @Operation(summary = "Adicionar médico", description = "Adiciona um novo médico")
     @PostMapping("/add")
     public ResponseEntity<Medico> addMedico(@RequestBody Medico medico) {
        return ResponseEntity.status(HttpStatus.CREATED).body(medicoService.addMedico(medico));
     }
 
+    @Operation(summary = "Editar médico", description = "Edita um médico existente")
     @PutMapping("/edit")
     public ResponseEntity<Medico> editMedico(@RequestBody Medico medico) {
        return ResponseEntity.status(HttpStatus.OK).body(medicoService.updateMedico(medico));
     }
 
+    @Operation(summary = "Excluir médico", description = "Exclui um médico com base no ID fornecido")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteMedico(@PathVariable("id") Long id) {
        medicoService.deleteMedico(id); 

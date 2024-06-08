bf328ea2d912356c280f6f7d0ca570289fc372a4
João Vitor Soares <jvfs12th@gmail.com>
2024-06-08 15:27:59 -0300

    :recycle: refactor: Adiciona tratamentos de erros para Médicos

--- src/main/java/com/group/vitalmedapi/services/MedicoService.java
+++ src/main/java/com/group/vitalmedapi/services/MedicoService.java
@@ -1,7 +1,6 @@
 package com.group.vitalmedapi.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,24 +10,41 @@ import com.group.vitalmedapi.repositories.MedicoRepository;
 
 @Service
 public class MedicoService {
-    
+
     @Autowired
     private MedicoRepository medicoRepository;
 
-    public List<Medico> findAll(){
-        return medicoRepository.findAll();
+    public List<Medico> findAll() {
+        List<Medico> medicos = medicoRepository.findAll();
+        if (medicos.isEmpty()) {
+            throw new RuntimeException("Nenhum médico encontrado.");
+        }
+        return medicos;
     }
 
-    public Optional<Medico> findById(Long id) {
-		return medicoRepository.findById(id);
-	}
+    public Medico findById(Long id) {
+        return medicoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Não existe médico com o ID: " + id));
+    }
 
     public Medico addMedico(Medico medico) {
-        return medicoRepository.save(medico);
+        try {
+            return medicoRepository.save(medico);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao adicionar médico", e);
+        }
     }
 
     public Medico updateMedico(Medico medico) {
-        return medicoRepository.save(medico);
+        if (!medicoRepository.existsById(medico.getId())) {
+            throw new RuntimeException("Médico não encontrado para o ID: " + medico.getId());
+        }
+
+        try {
+            return medicoRepository.save(medico);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao atualizar médico", e);
+        }
     }
 
     public void deleteMedico(Long id) {

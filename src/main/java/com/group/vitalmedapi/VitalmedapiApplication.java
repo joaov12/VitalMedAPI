7b4262c636e20bce463ac6aa191ef2d77db88557
João Vitor Soares <jvfs12th@gmail.com>
2024-06-09 21:03:58 -0300

    :recycle: refactor: Retira credenciais

--- src/main/java/com/group/vitalmedapi/VitalmedapiApplication.java
+++ src/main/java/com/group/vitalmedapi/VitalmedapiApplication.java
@@ -3,11 +3,15 @@ package com.group.vitalmedapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.group.vitalmedapi.config.DotenvApplicationInit;
+
 @SpringBootApplication
 public class VitalmedapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VitalmedapiApplication.class, args);
+		SpringApplication application = new SpringApplication(VitalmedapiApplication.class);
+        application.addInitializers(new DotenvApplicationInit());
+        application.run(args);
 	}
 
 }

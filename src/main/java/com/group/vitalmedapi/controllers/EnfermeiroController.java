9ef03230de3f4693eacb408e7fd3f6157ba60a2c
João Vitor Soares <jvfs12th@gmail.com>
2024-08-15 22:21:18 -0300

    :recycle: refactor: Altera nome da Tag de enfermeiros

--- src/main/java/com/group/vitalmedapi/controllers/EnfermeiroController.java
+++ src/main/java/com/group/vitalmedapi/controllers/EnfermeiroController.java
@@ -21,7 +21,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/enfermeiros")
-@Tag(name = "Enfermeiro")
+@Tag(name = "Enfermeiros")
 public class EnfermeiroController {
 
     @Autowired

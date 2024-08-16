ad83b178b47dd94f2a17cffafd430d71313701df
João Vitor Soares <jvfs12th@gmail.com>
2024-08-15 22:20:36 -0300

    :recycle: refactor: Altera nome da Tag de Cirurgias

--- src/main/java/com/group/vitalmedapi/controllers/ConsultaController.java
+++ src/main/java/com/group/vitalmedapi/controllers/ConsultaController.java
@@ -24,7 +24,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("consultas")
-@Tag(name = "Consulta")
+@Tag(name = "Consultas")
 public class ConsultaController {
 
     @Autowired
